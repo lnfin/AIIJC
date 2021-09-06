393c117faffdb7387d27e1787211439fe743355b
werserk <werserk@inbox.ru>
2021-09-06 21:06:54 +0300

    dice and focal losses

--- custom/losses.py
+++ custom/losses.py
@@ -1,4 +1,6 @@
 from torch import nn
+import torch
+import torch.nn.functional as F
 from torch.nn import BCELoss
 
 
@@ -20,3 +22,35 @@ class IoULoss(nn.Module):
         IoU = (intersection + smooth) / (union + smooth)
 
         return 1 - IoU
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(FocalLoss, self).__init__()
+
+    def forward(self, inputs, targets, alpha=0.8, gamma=2, smooth=1):
+        # flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        # first compute binary cross-entropy
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        BCE_EXP = torch.exp(-BCE)
+        focal_loss = alpha * (1 - BCE_EXP) ** gamma * BCE
+
+        return focal_loss
+
+
+class DiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        # flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        intersection = (inputs * targets).sum()
+        dice = (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
+
+        return 1 - dice
