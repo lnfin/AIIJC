5558b5bf0b1ce122cf02c53088c3d1ab4ab93cae
werserk <werserk@inbox.ru>
2021-09-07 14:36:40 +0300

    metric = mean from class metrics

--- custom/metrics.py
+++ custom/metrics.py
@@ -3,14 +3,16 @@ import torch
 
 
 class IoUScore(object):
-    def __init__(self, eps=1e-7, threshold=0.5):
-        self.threshold = threshold
+    def __init__(self, eps=1e-7, num_classes=4):
         self.eps = eps
+        self.num_classes = num_classes
 
     def __call__(self, y_pred, y_true):
         y_pred = torch.argmax(torch.sigmoid(y_pred), 1).float()
         y_true = torch.argmax(y_true, 1).float()
-        intersection = torch.sum(y_true == y_pred, dim=[1, 2])
-        union = torch.sum(y_true, dim=[1, 2]) + torch.sum(y_pred, dim=[1, 2]) - intersection
-        ious = ((intersection + self.eps) / (union + self.eps)).mean(dim=0)
-        return ious
+        ious = 0
+        for i in range(1, self.num_classes):
+            intersection = torch.sum((y_true * y_pred) == i, dim=[1, 2])
+            union = torch.sum(y_true, dim=[1, 2]) + torch.sum(y_pred, dim=[1, 2]) - intersection
+            ious += ((intersection + self.eps) / (union + self.eps)).mean(dim=0)
+        return ious / (self.num_classes - 1)
