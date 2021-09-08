560c1a347c5589f8dffc86e0865864bd367094b4
werserk <werserk@inbox.ru>
2021-09-08 13:37:23 +0300

    changed metric

--- custom/metrics.py
+++ custom/metrics.py
@@ -10,9 +10,7 @@ class IoUScore(object):
     def __call__(self, y_pred, y_true):
         y_pred = torch.argmax(torch.sigmoid(y_pred), 1).float()
         y_true = torch.argmax(y_true, 1).float()
-        ious = 0
-        for i in range(1, self.num_classes):
-            intersection = torch.sum((y_true * y_pred) == (i ** 2), dim=[1, 2])
-            union = torch.sum(y_true, dim=[1, 2]) + torch.sum(y_pred, dim=[1, 2]) - intersection
-            ious += ((intersection + self.eps) / (union + self.eps)).mean(dim=0)
-        return ious / (self.num_classes - 1)
+        intersection = torch.sum((y_true * y_pred) > 1, dim=[1, 2])
+        union = torch.sum(y_true, dim=[1, 2]) + torch.sum(y_pred, dim=[1, 2]) - intersection
+        iou = ((intersection + self.eps) / (union + self.eps)).mean(dim=0)
+        return iou
