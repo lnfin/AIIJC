8dc3c476c9a913ef8dff6bfdb54ceaf2854e9349
lnfin <dalmannikita@gmail.com>
2021-09-10 01:02:57 +0300

    скачивание сегментаций

--- app.py
+++ app.py
@@ -5,13 +5,19 @@ import numpy as np
 import custom.models
 from config import Cfg
 from production import get_predictions
+from zipfile import ZipFile
+import random
+import string
+import os
+import base64
+import cv2
 
 drive_link = 'https://drive.google.com/uc?id=1-tadxTBTRyru10rNNI0y4UcdntMK7hdh'
 
 
 @st.cache
 def download_model():
-    gdown.cached_download(drive_link, 'DeepLabV3_resnet101_multi.pth', quiet=False)
+    gdown.cached_download(drive_link, quiet=False)
 
 
 @st.cache
@@ -51,6 +57,8 @@ def main():
         unsafe_allow_html=True,
     )
     # download_model()
+    if not os.path.exists('segmentations/'):
+        os.mkdir('segmentations/')
 
     st.title('Сегментация поражения легких коронавирусной пневмонией')
 
@@ -64,25 +72,44 @@ def main():
         print(filenames)
         images = read_files(filenames)
         print(len(images))
-
-        st.info('Делаем предсказания, пожалуйста, подождите')
+        
+        user_dir = f"segmentations/{''.join(random.choice(string.ascii_lowercase) for _ in range(7))}/"
+        os.mkdir(user_dir)
+        
         cfg = Cfg(multi_class)
+        zip_obj = ZipFile('segmentations.zip', 'w')
         with st.expander("Информация о каждом фото"):
-            for filename, pred in zip(images, get_predictions(cfg, images)):
+            info = st.info('Делаем предсказания, пожалуйста, подождите')
+            for filename, pred in zip(images, get_predictions(cfg, images)):    
+                info.empty()
                 st.markdown(f'<h3>{filename}</h3>', unsafe_allow_html=True)
 
                 original = np.array(Image.open(filename))
                 col1, col2 = st.columns(2)
                 col1.header("Оригинал")
                 col1.image(original, width=350)
-
+                
+                to_img = pred * 255
+                cv2.imwrite(user_dir + filename.split('/')[1], to_img)
+                zip_obj.write(user_dir + filename.split('/')[1])
+                
                 col2.header("Сегментация")
                 col2.image(pred, width=350)
 
                 st.markdown('<br />', unsafe_allow_html=True)
 
+            zip_obj.close()
+            
         with st.expander("Скачать сегментации"):
-            pass
+            with open('segmentations.zip', 'rb') as file:
+                st.download_button(
+                    label="Архив сегментаций",
+                    data=file,
+                    file_name="segmentations.zip",
+            )
+        
+        # os.rmdir(user_dir)
+        # os.rmdir(user_dir)
 
 
 if __name__ == '__main__':
