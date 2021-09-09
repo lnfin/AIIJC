4217b57e92a4ee5f575f6cbbe44f761e8739cf48
werserk <werserk@inbox.ru>
2021-09-09 11:48:37 +0300

    config for production

--- config.py
+++ config.py
@@ -1,38 +1,17 @@
 class Cfg:
     seed = 0xD153A53
 
-    # DeepLabV3
-    model = 'DeepLabV3'
+    best_dict = "DeepLabV3_resnet101" + '.pth'
+    model = best_dict.split('_')[0]
 
     in_channels = 1
-    best_dict = "DeepLabV3_resnet101" + '.pth'
     output_channels = 2
-    backbone = 'resnet101'  # 50, 101
+    backbone = best_dict.split('.')[0].split('_')[1]
     pretrained = True
 
-    # TRAIN AND EVAL SETTINGS
-    lr = 1e-4
-    epochs = 20
-    batch_size = 1
-
-    train_size, val_size = 0.8, 0.2
-    # CUSTOM
-    metric = 'IoUScore'
-
-    criterion = 'IoULoss'
-    optimizer = 'Adam'
-    # scheduler = 'OneCycleLR'
-
-    # PATHES
     root_folder = '/mnt/c/Users/dalma/Desktop/AIIJC/CovidSeg/'
 
-    data_folder = 'data/'
-    dataset_name = 'MosMed'  # MosMed, Zenodo, ZenodoLungs, MedSeg,
-
-    custom_folder = 'custom/'
-
-    # AUGMENTATIONS AND TRANSFORMS
-    pre_transforms = [  # Pre-transforms
+    pre_transforms = [
         dict(
             name="Resize",
             params=dict(
@@ -43,19 +22,5 @@ class Cfg:
         ),
     ]
 
-    augmentations = [  # Augmentations
-        dict(
-            name="HorizontalFlip",
-            params=dict(
-                p=0.5,
-            )
-        )]
-
-    post_transforms = [  # Post-transforms
-        ]
-
-    # CROSS-VALIDATION
-    kfold = False
-
-    n_splits = 5
-    fold_number = 1  # from 1 to n_splits
+    augmentations = []
+    post_transforms = []
