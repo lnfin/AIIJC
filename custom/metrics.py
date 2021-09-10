1093ed6171368b203eb319964fc1a174c0daf125
werserk <werserk@inbox.ru>
2021-09-10 12:22:57 +0300

    deleted debug in metric

--- custom/metrics.py
+++ custom/metrics.py
@@ -9,8 +9,6 @@ class IoUScore(object):
     def __call__(self, y_pred, y_true):
         y_pred = torch.argmax(torch.sigmoid(y_pred), 1).float()
         y_true = torch.argmax(y_true, 1).float()
-        print(y_pred.shape, y_true.shape)
-        print(torch.unique(y_pred), torch.unique(y_true))
         intersection = torch.sum((y_true * y_pred) > 1, dim=[1, 2])
         union = torch.sum(y_true, dim=[1, 2]) + torch.sum(y_pred, dim=[1, 2]) - intersection
         iou = ((intersection + self.eps) / (union + self.eps)).mean(dim=0)
