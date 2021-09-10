3fd4f86c97a6eb37f4df03b7a5008b0a685ee1c0
werserk <werserk@inbox.ru>
2021-09-10 13:11:14 +0300

    old metric

--- custom/metrics.py
+++ custom/metrics.py
@@ -1,4 +1,5 @@
 import torch
+from utils import discretize_segmentation_maps
 
 
 class IoUScore(object):
@@ -13,3 +14,17 @@ class IoUScore(object):
         union = torch.sum(y_true, dim=[1, 2]) + torch.sum(y_pred, dim=[1, 2]) - intersection
         iou = ((intersection + self.eps) / (union + self.eps)).mean(dim=0)
         return iou
+
+
+class IoUScoreBinary(object):
+    def __init__(self, eps=1e-7, threshold=0.5):
+        self.threshold = threshold
+        self.eps = eps
+
+    def __call__(self, y_pred, y_true):
+        y_pred = discretize_segmentation_maps(y_pred, self.threshold).float()
+        y_true = y_true.float()
+        intersection = torch.sum(y_true * y_pred, dim=[2, 3])
+        union = torch.sum(y_true, dim=[2, 3]) + torch.sum(y_pred, dim=[2, 3]) - intersection
+        ious = ((intersection + self.eps) / (union + self.eps)).mean(dim=0)
+        return ious
