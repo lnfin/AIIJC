604e36330f353019838c1e1491b7eda45294ad92
werserk <werserk@inbox.ru>
2021-09-20 22:30:23 +0300

    comments

--- production.py
+++ production.py
@@ -33,36 +33,42 @@ def get_setup():
     return models, transforms
 
 
-def get_folder_name():
+def generate_folder_name():
     return ''.join(random.choice(string.ascii_lowercase) for _ in range(7)) + '/'
 
 
 def data_to_paths(data, save_folder):
     all_paths = []
-    if not os.path.isdir(data):
+    if not os.path.isdir(data):  # single file
         data = [data]
-    else:
+    else:  # folder of files
         data = [os.path.join(data, x) for x in os.listdir(data)]
+
     for path in data:
-        print(path)
-        if not os.path.exists(path):
+        if not os.path.exists(path):  # path not exists
             print(f'Path \"{path}\" not exists')
             continue
+        # reformatting by type
         if path.endswith('.png') or path.endswith('.jpg') or path.endswith('.jpeg'):
             all_paths.append(path)
         elif path.endswith('.nii') or path.endswith('.nii.gz'):
+            # NIftI format will be png format in folder "slices"
             if not os.path.exists(os.path.join(save_folder, 'slices')):
                 os.mkdir(os.path.join(save_folder, 'slices'))
+
             paths = []
+
+            # NIftI to numpy arrays
             nii_name = path.split('\\')[-1].split('.')[0]
             images = nib.load(path)
             images = np.array(images.dataobj)
             images = np.moveaxis(images, -1, 0)
 
             for i, image in enumerate(images):
-                image = window_image(image, -600, 1500)
+                image = window_image(image)  # windowing
                 image += abs(np.min(image))
                 image = image / np.max(image)
+                # saving like png image
                 image_path = os.path.join(save_folder, 'slices', nii_name + '_' + str(i) + '.png')
                 cv2.imwrite(image_path, image * 255)
 
@@ -73,7 +79,7 @@ def data_to_paths(data, save_folder):
     return all_paths
 
 
-def window_image(image, window_center, window_width):
+def window_image(image, window_center=-600, window_width=1500):
     img_min = window_center - window_width // 2
     img_max = window_center + window_width // 2
     window_image = image.copy()
@@ -83,28 +89,37 @@ def window_image(image, window_center, window_width):
 
 
 def read_files(files):
-    folder_name = get_folder_name()
+    # creating folder for user
+    folder_name = generate_folder_name()
     path = 'images/' + folder_name
     if not os.path.exists(path):
         os.mkdir(path)
-    imgs = list()
+
+    imgs = []
     for file in files:
         imgs.append([])
+        # if NIfTI we should get slices
         if file.name.endswith('.nii'):
+            # saving file from user
             nii_path = path + file.name
             open(nii_path, 'wb').write(file.getvalue())
+
+            # loading
             images = nib.load(nii_path)
             images = np.array(images.dataobj)
             images = np.moveaxis(images, -1, 0)
-            os.remove(nii_path)
 
-            for image in images:
-                image = window_image(image, -600, 1500)
+            os.remove(nii_path)  # clearing
+
+            for image in images:  # saving every slice in NIftI
+                # windowing
+                image = window_image(image)
                 image += abs(np.min(image))
                 image = image / np.max(image)
+
+                # saving
                 image_path = path + file.name.split('.')[0] + '.png'
                 cv2.imwrite(image_path, image * 255)
-
                 imgs[-1].append(image_path)
 
         else:
@@ -121,9 +136,12 @@ def create_folder(path):
 
 
 def get_predictions(paths, models, transforms, multi_class=True):
+    # preparing
     binary_model, multi_model, lung_model = models
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     dataloader = DataLoader(ProductionCovid19Dataset(paths, transform=transforms[0]), batch_size=1, drop_last=False)
+
+    # prediction
     for X, _ in dataloader:
         X = X.to(device)
         X = X / torch.max(X)
@@ -138,6 +156,7 @@ def get_predictions(paths, models, transforms, multi_class=True):
             lung = lung.squeeze().cpu()
             lung = torch.argmax(lung, 0).float()
 
+            # if multi class we should use both models to predict
             if multi_class:
                 multi_output = multi_model(X)
                 multi_pred = multi_output.squeeze().cpu()
@@ -157,7 +176,7 @@ def make_masks(paths, models, transforms, multi_class=True):
 
         lung_sum = np.sum(lung)  # lung pixels
 
-        if lung_sum == 0:  # Костыль-фича
+        if lung_sum == 0:  # Feature
             lung_sum = img.shape[1] * img.shape[2]
             lung_sum = lung_sum / 3.5
             print('lungs not found')
