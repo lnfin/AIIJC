e3b58d82ccce9fd344b45a42ea6a57229d82e966
werserk <werserk@inbox.ru>
2021-09-20 23:27:21 +0300

    cosmetics and comments

--- custom/metrics.py
+++ custom/metrics.py
@@ -1,10 +1,4 @@
 import torch
-import numpy as np
-
-
-def discretize_segmentation_maps(probs, threshold=0.5):
-    threshold = torch.from_numpy(np.array(threshold)).to(probs.device)
-    return probs > threshold
 
 
 class IoUScore(object):
@@ -21,17 +15,3 @@ class IoUScore(object):
         union = torch.sum(y_true, dim=[1, 2]) + torch.sum(y_pred, dim=[1, 2]) - intersection
         iou = ((intersection + self.eps) / (union + self.eps)).mean(dim=0)
         return iou
-
-
-class IoUScoreBinary(object):
-    def __init__(self, eps=1e-7, threshold=0.5):
-        self.threshold = threshold
-        self.eps = eps
-
-    def __call__(self, y_pred, y_true):
-        y_pred = discretize_segmentation_maps(y_pred, self.threshold).float()
-        y_true = y_true.float()
-        intersection = torch.sum(y_true * y_pred, dim=[1, 2])
-        union = torch.sum(y_true, dim=[1, 2]) + torch.sum(y_pred, dim=[1, 2]) - intersection
-        ious = ((intersection + self.eps) / (union + self.eps)).mean(dim=0)
-        return ious
--- main.py
+++
@@ -1,7 +0,0 @@
-from config import Cfg
-from utils import set_seed
-from train_functions import run
-
-set_seed(Cfg.seed)
-
-run(Cfg)
